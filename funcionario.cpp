16f0df158adf23bc5a3a2dab31585076c00e34af
Thyago Monteiro <tmacademico@gmail.com>
2019-10-14 15:38:31 -0300

    Adicionando arquivos principais

---
+++ funcionario.cpp
@@ -0,0 +1,426 @@
+#include <iostream>
+#include <mysql.h>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+class funcionario {
+public:
+    bool fazerLogin (MYSQL *servidor, string login, string senha){
+            mysql_select_db(servidor,"hoteldb")==0;
+        if (true) {
+            string query = "SELECT id FROM funcionario WHERE nome='"+login+"' AND senha = '"+ senha+"';";
+            mysql_query(servidor,query.c_str());
+            if (mysql_errno(servidor)==0) {
+               MYSQL_RES* res = mysql_use_result(servidor);
+                MYSQL_ROW row;
+                while( ( row = mysql_fetch_row(res)) != NULL ) {
+                    string c_senha = row[1];
+                    if (c_senha == senha){
+                         cout << "TESTE A";
+                         return true;
+
+                    } else {
+                         cout << "TESTE B";
+                        return false;
+
+                    }
+                }
+            }
+            getchar();
+        } else {
+             cout<<"\nErro ao acessar o banco de dados "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            return false;
+
+        }
+    }
+
+    void cabecalho() {
+        cout<< " --- HOTEL DO FIM DO MUNDO ---\n";
+        cout<< "   Quintos dos Infernos, 666\n";
+        cout<< "   Telefone: (66) 66669-6969\n";
+        cout<< " -----------------------------\n\n";
+
+    }
+    void cadastrarFuncionarioBD(MYSQL *servidor, string nome, string cpf, string celular, string telefone, string uf, string data_nascimento, string cidade, string nacionalidade, string salario ) {
+        mysql_select_db(servidor,"hoteldb");
+        if (mysql_errno(servidor)==0) {
+            string query = "INSERT INTO funcionario (nome, cpf, celular, telefone, uf, data_nascimento, cidade, nacionalidade, salario ) VALUES('"+(nome)+"', '"+(cpf)+"', '"+(celular)+"','"+(telefone)+"', '"+(uf)+"', '"+(data_nascimento)+"', '"+(cidade)+"', '"+(nacionalidade)+"', '"+(salario)+"');";
+            mysql_query(servidor,query.c_str());
+
+            if (mysql_errno(servidor)==0) {
+                cout << "Hóspede cadastrado com sucesso.\nPressione a tecla ENTER para continuar.";
+            }
+            getchar();
+        } else {
+            cout<<"\nErro ao acessar o banco de dados "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            exit(1);
+        }
+    }
+
+    void cadastrarFuncionario(MYSQL *servidor) {
+        string nome, cpf, celular, telefone, uf, data_nascimento, cidade, nacionalidade, salario;
+        int confirma; // Resposta de confirmação
+        do {
+            system("CLS");
+            cout<< "--- Cadastrar Funcionário ---\n";
+            cout<< "Digite o nome do Funcionário:";
+            cin>>nome;
+            cout<< "Digite o CPF do Funcionário:";
+            cin>>cpf;
+            cout<< "Digite o Celular do Funcionário:";
+            cin>>celular;
+            cout<< "Digite o telefone do Funcionário:";
+            cin>>telefone;
+            cout<< "Digite a data de nascimento do Funcionário:";
+            cin>>data_nascimento;
+            cout<< "Digite a nacionalidade do Funcionário:";
+            cin>>nacionalidade;
+            cout<< "Digite a cidade do Funcionário:";
+            cin>>cidade;
+            cout<< "Digite a UF do Funcionário:";
+            cin>>uf;
+            cout<< "Digite o Salário do Funcionário:";
+            cin>>salario;
+            do {
+                cout<< "\n\nVocê confirma os dados acima?\n Digite 1 para SIM, 2 para NÃO, 3 para CANCELAR CADASTRO: ";
+                cin>>confirma;
+                switch (confirma) {
+                case 1: {
+                    cadastrarFuncionarioBD(servidor, nome, cpf, celular, telefone, uf, data_nascimento, cidade, nacionalidade, salario);
+                    break;
+                }
+                case 2: {
+                    cout<< "\n\nReiniciando cadastro";
+                    for(int i = 0; i<5; i++) {
+                        cout<<".";
+                        Sleep(500);
+                        break;
+                    }
+                }
+                case 3: {
+                    cout<<"\n\nCadastro cancelado com sucesso!\n\n Pressione ENTER para retornar à tela inicial";
+                    getchar();
+                    break;
+                }
+                default: {
+                    cout<<"\n\nOpção inválida. Tente novamente! \n\n Pressione ENTER para continuar";
+                    getchar();
+                }
+                }
+            } while (confirma<1 && confirma > 3);
+        } while (confirma != 1 && confirma!= 3);
+    }
+
+    void procurarPorCPFBD(MYSQL *servidor, string cpf) {
+        mysql_select_db(servidor,"hoteldb");
+        if (mysql_errno(servidor)==0) {
+            string query = "SELECT id, nome, cpf  from funcionario WHERE cpf LIKE '%"+cpf+"%';";
+            mysql_query(servidor,query.c_str());
+
+            cout << "Pesquisando";
+            for(int i = 1; i<5; i++) {
+                Sleep(500);
+                cout << ".";
+            }
+
+            if (mysql_errno(servidor)==0) {
+                cout << "\nPesquisa realizada com sucesso!\n";
+                Sleep(1);
+                MYSQL_RES* res = mysql_use_result(servidor);
+                MYSQL_ROW row;
+                int i = 0;
+                while( ( row = mysql_fetch_row(res)) != NULL ) {
+                    cout <<"\n\t"<< ++i<<". ID: "<<row[0] << " Nome: "<< row[1] <<"\t CPF: "<< row[2] << endl;
+                    Sleep(500);
+                }
+                cout << "Foram encontrados " << i << " resultado(s)";
+                getchar();
+            }
+        } else {
+            cout<<"\nErro ao acessar o banco de dados "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            exit(1);
+        }
+
+        cout<<"\nPresssione ENTER para continuar";
+    }
+
+    void procurarPorNomeBD(MYSQL *servidor, string nome) {
+        mysql_select_db(servidor,"hoteldb");
+        if (mysql_errno(servidor)==0) {
+            string query = "SELECT id, nome, cpf from funcionario WHERE nome LIKE \'%%"+nome+"%%\';";
+            mysql_query(servidor,query.c_str());
+
+            cout << "Pesquisando";
+            for(int i = 1; i<5; i++) {
+                Sleep(500);
+                cout << ".";
+            }
+
+            if (mysql_errno(servidor)==0) {
+                cout << "\nPesquisa realizada com sucesso!\n";
+                Sleep(1);
+                MYSQL_RES* res = mysql_use_result(servidor);
+                MYSQL_ROW row;
+                int i = 0;
+                while( ( row = mysql_fetch_row(res)) != NULL ) {
+                    cout <<"\n\t"<< ++i<<". ID: "<<row[0] << " Nome: "<< row[1] <<"\t CPF: "<< row[2] << endl;
+                    Sleep(500);
+                }
+                cout << "Foram encontrados " << i << " resultado(s)";
+                getchar();
+            }
+        } else {
+            cout<<"\nErro ao acessar o banco de dados "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            exit(1);
+        }
+
+        cout<<"\nPresssione ENTER para continuar";
+    }
+
+    void pesquisarFuncionario(MYSQL *servidor) {
+        system("CLS");
+        int op;
+        do {
+            cout<< "---  PROCURAR HÓSPEDE  ---\n";
+            cout<< "\nDigite 1 para pesquisar por NOME\nDigite 2 para pesquisar por CPF\nDigite 3 para cancelar";
+            cin>>op;
+            switch(op) {
+            case 1: {
+                string nome;
+                cout<< "\nDigite o nome desejado:\n";
+                cin>>nome;
+                procurarPorNomeBD(servidor, nome);
+                break;
+
+            }
+            case 2: {
+                string cpf;
+                cout<< "\nDigite o cpf desejado:\n";
+                cin>>cpf;
+                procurarPorNomeBD(servidor, cpf);
+                break;
+
+            }
+            case 3: {
+                cout<<"Operação Cancelada";
+            }
+            default: {
+                cout<<"Opção inválida. Tente novamente";
+            }
+            }
+        } while(op<1&&op>3);
+    }
+
+    void editarFuncionario(MYSQL *servidor) {
+        system("CLS");
+        mysql_select_db(servidor,"hoteldb");
+        string id;
+        if (mysql_errno(servidor)==0) {
+            cout << "\n---  EDITAR Hóspede  ---\n";
+            cout << "\n\nDigite a ID do hóspede a ser editado: ";
+            cin >> id;
+            string query = "SELECT id, nome,cpf from funcionario WHERE id LIKE '%"+id+"%';";
+            mysql_query(servidor,query.c_str());
+
+            cout << "Aguarde";
+            for(int i = 1; i<10; i++) {
+                Sleep(500);
+                cout << ".";
+            }
+
+            if (mysql_errno(servidor)==0) {
+                cout << "\nPronto!\n";
+                Sleep(1);
+                MYSQL_RES* res = mysql_use_result(servidor);
+                MYSQL_ROW row;
+                int i = 0;
+                while( ( row = mysql_fetch_row(res)) != NULL ) {
+                    cout <<"\n\t"<< ++i<<". ID: "<<row[0] << "Nome: "<< row[1] <<"\t CPF: "<< row[2] << endl;
+                    Sleep(500);
+                }
+                cout << "Foram encontrados " << i << " resultado(s)";
+                getchar();
+            }
+
+        } else {
+            cout<<"\nErro ao acessar o banco de dados "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            exit(1);
+        }
+        string nome;
+        cout<< "\nEditar usuário com a id:" <<id<<"\n";
+        cout<< "Editando o nome do hóspede:\n";
+
+        cout<< "Digite o novo nome do hóspede:";
+        cin>>nome;
+
+        mysql_select_db(servidor,"hoteldb");
+        if (mysql_errno(servidor)==0) {
+            string query = "UPDATE funcionario SET nome='"+(nome)+"' WHERE id = "+(id)+";";
+            mysql_query(servidor,query.c_str());
+
+            if (mysql_errno(servidor)==0) {
+                cout << "Hóspede editado com sucesso.\nPressione a tecla ENTER para continuar.";
+            }
+            getchar();
+        } else {
+            cout<<"\nErro ao acessar o banco de dados "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            exit(1);
+        }
+
+        cout<<"\nPresssione ENTER para continuar";
+    }
+
+
+    void excluirFuncionario(MYSQL *servidor) {
+        system("CLS");
+        mysql_select_db(servidor,"hoteldb");
+        string id;
+        if (mysql_errno(servidor)==0) {
+            cout << "---  EXCLUIR Hóspede  ---\n";
+            cout << "\n\nDigite a ID do hóspede a ser excluído: ";
+            cin >> id;
+            string query = "SELECT id, nome,cpf from funcionario WHERE id LIKE '%"+id+"%';";
+            mysql_query(servidor,query.c_str());
+
+            cout << "Aguarde";
+            for(int i = 1; i<10; i++) {
+                Sleep(500);
+                cout << ".";
+            }
+
+            if (mysql_errno(servidor)==0) {
+                cout << "\nPronto!\n";
+                Sleep(1);
+                MYSQL_RES* res = mysql_use_result(servidor);
+                MYSQL_ROW row;
+                int i = 0;
+                while( ( row = mysql_fetch_row(res)) != NULL ) {
+                    cout <<"\n\t"<< ++i<<". ID: "<<row[0] << "Nome: "<< row[1] <<"\t CPF: "<< row[2] << endl;
+                    Sleep(500);
+                }
+                cout << "Foram encontrados " << i << " resultado(s)";
+                getchar();
+            }
+
+        } else {
+            cout<<"\nErro ao acessar o banco de dados "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            exit(1);
+        }
+        string nome;
+        cout<< "\nExcluir usuário com a id:" <<id<<"\n";
+        cout<< "Confirma a exclusão?\n";
+
+        cout<< "Digite o novo nome do hóspede:";
+        cin>>nome;
+
+        mysql_select_db(servidor,"hoteldb");
+        if (mysql_errno(servidor)==0) {
+            string query = "UPDATE funcionario SET nome='"+(nome)+"' WHERE id = "+(id)+";";
+            mysql_query(servidor,query.c_str());
+
+            if (mysql_errno(servidor)==0) {
+                cout << "Hóspede editado com sucesso.\nPressione a tecla ENTER para continuar.";
+            }
+            getchar();
+        } else {
+            cout<<"\nErro ao acessar o banco de dados "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            exit(1);
+        }
+    }
+
+    void contarFuncionarios(MYSQL *servidor) {
+        system("CLS");
+        cout << "---  Contagem total de hóspedes  ---\n";
+        mysql_select_db(servidor,"hoteldb");
+        if (mysql_errno(servidor)==0) {
+            string query = "SELECT COUNT(*) FROM funcionario;";
+            mysql_query(servidor,query.c_str());
+
+            cout << "Contando";
+            for(int i = 1; i<10; i++) {
+                Sleep(500);
+                cout << ".";
+            }
+        }
+
+        if (mysql_errno(servidor)==0) {
+            cout << "\nPronto!\n";
+            Sleep(1);
+            MYSQL_RES* res = mysql_use_result(servidor);
+            MYSQL_ROW row;
+            while( ( row = mysql_fetch_row(res)) != NULL ) {
+                cout <<" Total de hóspedes cadastrados: "<<row[0] <<  endl;
+            }
+
+            cout<< "Pressione ENTER para continuar";
+            getchar();
+        }
+    }
+
+    void listarporUF(MYSQL *servidor) {
+        mysql_select_db(servidor,"hoteldb");
+        if (mysql_errno(servidor)==0) {
+            system("CLS");
+            cout << "---  Listando hóspedes por UF  ---\n";
+
+            string query = "SELECT id, nome, uf from funcionario ORDER BY uf;";
+            mysql_query(servidor,query.c_str());
+
+            cout << "\nListando";
+            for(int i = 1; i<10; i++) {
+                Sleep(500);
+                cout << ".";
+            }
+
+            if (mysql_errno(servidor)==0) {
+                cout << "\nPesquisa realizada com sucesso!\n";
+                Sleep(1);
+                MYSQL_RES* res = mysql_use_result(servidor);
+                MYSQL_ROW row;
+                int i = 0;
+                while( ( row = mysql_fetch_row(res)) != NULL ) {
+                    cout <<"\n\t"<< ++i<<". ID: "<<row[0] << " Nome: "<< row[1] <<"\t UF: "<< row[2] << endl;
+                    Sleep(500);
+                    getchar();
+                }
+                cout << "Foram encontrados " << i << " resultado(s)";
+                getchar();
+            }
+        } else {
+            cout<<"\nErro ao acessar o banco de dados "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            exit(1);
+        }
+
+        cout<<"\nPresssione ENTER para continuar";
+        getchar();
+    }
+
+    void verificarCadastros(MYSQL *servidor) {
+
+        mysql_select_db(servidor,"hoteldb");
+        if (mysql_errno(servidor)==0) {
+            string query = "SELECT COUNT(*) FROM funcionario;";
+            mysql_query(servidor,query.c_str());
+        }
+        int i;
+        if (mysql_errno(servidor)==0) {
+            MYSQL_RES* res = mysql_use_result(servidor);
+            MYSQL_ROW row;
+            while( ( row = mysql_fetch_row(res)) != NULL ) {
+                cout<< row;
+                i = (int)row[0]-48;
+            }
+        }
+        cout << i;
+        if (i==0) {
+
+            cadastrarFuncionario(servidor);
+        }
+        getchar();
+    }
+
+};
---
+++ main.cpp
@@ -0,0 +1,240 @@
+#include <iostream>
+#include <conio.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include "funcionario.cpp"
+#include <mysql.h>
+
+using namespace std;
+
+MYSQL  *servidor;
+
+void conectar() {
+
+    servidor= mysql_init(0);
+
+    if(servidor==NULL) {
+        cout<< "Verificando Banco de Dados...\n";
+        getchar();
+        exit(1);
+    } else {
+        if ( mysql_real_connect(servidor, "localhost", "root", "", NULL, 0, NULL, 0) ) {
+            cout<<"\nBanco de dados conectado com sucesso!\n";
+            mysql_autocommit ( servidor, 1 ) ;
+        } else {
+            cout<<"\nFalha ao conectar ao banco de dados\n";
+            cout<<"Erro: BD01 "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+            getchar();
+            mysql_close(servidor);
+            exit(1);
+        }
+    }
+}
+
+void verificarEstruturaBD() {
+
+    cout<<"\nVerificando Banco de Dados...\n";
+    mysql_query(servidor, "CREATE DATABASE IF NOT EXISTS hoteldb");
+    if(mysql_errno(servidor)==0) {
+        cout<< "Banco de Dados criado com sucesso\n\n" ;
+    } else {
+        cout<<"\nErro na Criação do Bando de Dados: hotelbd "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+        exit(1);
+    }
+    mysql_select_db(servidor,"hoteldb");
+
+    cout<<"\nVerificando estrutura das tabelas...\n";
+
+    //Criando estrutura da tabela HÓSPEDE
+    mysql_query(servidor, "CREATE TABLE IF NOT EXISTS hospede (id int PRIMARY KEY AUTO_INCREMENT, nome varchar(120) NOT NULL, cpf varchar(14) UNIQUE NOT NULL , celular varchar(11), telefone varchar(11), uf varchar(2), data_nascimento date, cidade varchar (50), nacionalidade varchar(50))");
+    if (mysql_errno(servidor)==0) {
+        cout<< "Tabela HOSPEDE Criada com Sucesso!\n";
+    } else {
+        cout<<"\nErro na Criação da tabela no BD "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+        exit(1);
+    }
+
+    //Criando a estrutura da tabela da suite
+    mysql_query(servidor, "CREATE TABLE IF NOT EXISTS suite (id int PRIMARY KEY AUTO_INCREMENT, numero SMALLINT UNIQUE NOT NULL, vagas SMALLINT(2)  , status varchar(15) DEFAULT ('N'), preco DECIMAL(6,2))");
+    if (mysql_errno(servidor)==0) {
+        cout<< "Tabela SUITE Criada com Sucesso!\n";
+    } else {
+        cout<<"\nErro na Criação da tabela no BD "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+        exit(1);
+    }
+
+    //criando a estrutura da tabela de funcionário
+    mysql_query(servidor, "CREATE TABLE IF NOT EXISTS funcionario (id int PRIMARY KEY AUTO_INCREMENT, nome varchar(120) NOT NULL, cpf varchar(14) UNIQUE NOT NULL , celular varchar(11), telefone varchar(11)  , uf varchar(2), data_nascimento date, cidade varchar (50), nacionalidade varchar(50), salario DECIMAL(6,2), atributo varchar(10), senha varchar(6))");
+    if (mysql_errno(servidor)==0) {
+        cout<< "Tabela FUNCIONARIO Criada com Sucesso!\n";
+        mysql_query(servidor, "INSERT INTO funcionario (nome, cpf, senha) VALUES ('admin', '00000000000' ,'admin');");
+    } else {
+        cout<<"\nErro na Criação da tabela no BD "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+        exit(1);
+    }
+
+    //criando a estrutura da tabela de hospedagem
+    mysql_query(servidor, "CREATE TABLE IF NOT EXISTS hospedagem (id int PRIMARY KEY AUTO_INCREMENT, id_hospede int, id_suite int, id_funcionario int, data_entrada date, data_saida date, status varchar(20), conta_final DECIMAL(6,2), forma_pagamento varchar(10), senha varchar(6))");
+    mysql_query(servidor, "ALTER TABLE hospedagem ADD FOREIGN KEY (id_hospede) REFERENCES hospede(id);");
+    mysql_query(servidor, "ALTER TABLE hospedagem ADD FOREIGN KEY (id_suite) REFERENCES suite(id);");
+    mysql_query(servidor, "ALTER TABLE hospedagem ADD FOREIGN KEY (id_funcionario) REFERENCES funcionario(id);");
+
+    if (mysql_errno(servidor)==0) {
+        cout<< "Tabela FUNCIONARIO Criada com Sucesso!\n";
+    } else {
+        cout<<"\nErro na Criação da tabela no BD "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+        exit(1);
+    }
+
+    //criando a estrutura da tabela de serviços
+    mysql_query(servidor, "CREATE TABLE IF NOT EXISTS servicos (id int PRIMARY KEY AUTO_INCREMENT, id_hospedagem int, id_funcionario int, data_entrada date, data_saida date, status varchar(20), valor DECIMAL(6,2))");
+    mysql_query(servidor, "ALTER TABLE servicos ADD FOREIGN KEY (id_funcionario) REFERENCES funcionario(id);");
+    mysql_query(servidor, "ALTER TABLE servicos ADD FOREIGN KEY (id_hospedagem) REFERENCES hospedagem(id);");
+    if (mysql_errno(servidor)==0) {
+        cout<< "Tabela FUNCIONARIO Criada com Sucesso!\n";
+    } else {
+        cout<<"\nErro na Criação da tabela no BD "<< mysql_errno(servidor) << ", Mensagem: " << mysql_error(servidor)<<endl;
+        exit(1);
+    }
+
+
+}
+
+void gotoxy(int coluna, int linha)
+{
+ COORD point;
+ point.X = coluna; point.Y = linha;
+ SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), point);
+}
+
+void textcolor(int color) {
+// 0 1 2 3 4 5 6 7 8 ...
+//typedef enum{BLACK,BLUE,GREEN,CYAN,RED,MAGENTA,BROWN,LIGHTGRAY,DARKGRAY,
+//LIGHTBLUE,LIGHTGREEN,LIGHTCYAN,LIGHTRED,LIGHTMAGENTA,YELLOW,WHITE} COLORS;
+// incluir a biblioteca windows.h
+// este método geralmente se encontra na biblioteca conio.h
+// caso não esteja na biblioteca, use este metodo
+int __BACKGROUND = 0;
+SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),color + (__BACKGROUND << 4));
+}
+
+void desenharRetangulo (int base, int altura){
+	//Desenhar um retângulo
+	printf("%c",201);
+	for (int i = 2; i<base; i++){
+		printf("%c",205);
+	}
+	printf("%c\n",187);
+
+	for (int j = 2; j < altura; j++){
+
+		printf("%c",186);
+		for (int i = 2; i<base; i++){
+			printf("%c",32);
+		}
+	printf("%c\n",186);
+
+	}
+
+	printf("%c",200);
+	for (int i = 2; i<base; i++){
+		printf("%c",205);
+	}
+	printf("%c\n",188);
+}
+
+void desenharRetangulo2 (int base, int altura, int x, int y){
+	//Desenhar um retângulo
+	gotoxy(x, y++);
+	printf("%c",201);
+	for (int i = 2; i<base; i++){
+		printf("%c",205);
+	}
+	printf("%c\n",187);
+
+
+	for (int j = 2; j < altura; j++){
+		gotoxy(x, y++);
+		printf("%c",186);
+		for (int i = 2; i<base; i++){
+			printf("%c",32);
+		}
+	printf("%c\n",186);
+
+	}
+
+	gotoxy(x, y++);
+	printf("%c",200);
+	for (int i = 2; i<base; i++){
+		printf("%c",205);
+	}
+	printf("%c\n",188);
+}
+
+void margemTela(){
+    textcolor(5);
+    system("CLS");
+    desenharRetangulo(119,29);
+}
+
+void telaLogin(){
+    string login, senha;
+    margemTela();
+    textcolor(3);
+    desenharRetangulo2(50,15,33,7);
+    textcolor(2);
+    gotoxy(47, 9);
+    printf("SISTEMA DE HOTELARIA");
+    gotoxy(40, 12);
+    printf("LOGIN:");
+    gotoxy(40, 14);
+    printf("SENHA:");
+    gotoxy(40, 19);
+    printf("Pressione ENTER para continuar");
+
+    //Login
+    bool logincheck;
+     funcionario f;
+    do {
+        textcolor(1);
+        gotoxy(47, 12);
+        cin >> login;
+        gotoxy(47, 14);
+        cin >> senha;
+
+        logincheck = NULL;
+        logincheck = f.fazerLogin(servidor, login, senha);
+        gotoxy(47, 16);
+            textcolor(2);
+            printf("       Aguarde...        ");
+            Sleep(500);
+        if( !logincheck){
+            textcolor(4);
+            gotoxy(47, 12);
+            printf("                    ");
+            gotoxy(47, 14);
+            printf("                    ");
+            gotoxy(47, 16);
+            printf("LOGIN OU SENHA INCORRETOS");
+            gotoxy(47, 17);
+            printf("    TENTE NOVAMENTE!");
+            cout << login << "  " << senha;
+        }
+    } while (!logincheck);
+    gotoxy(0,29);
+
+}
+
+int main()
+{
+    conectar();
+    verificarEstruturaBD();
+    setlocale(LC_ALL, "pt_BR_utf8");
+    telaLogin();
+
+    gotoxy(0,29);
+    return 0;
+}
